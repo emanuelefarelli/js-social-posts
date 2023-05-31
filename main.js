b67e1eeb9d6583ac79e823daceed3400dd1fc633
emanuelefarelli <emaaliens888@gmail.com>
2023-05-31 15:44:44 +0200

    Reached milestone 1

--- main.js
+++ main.js
@@ -59,6 +59,38 @@ const myContainer = document.getElementById('container');
 
 posts.forEach((post) => {
     const domPost = document.createElement('div');
+    const {content, media, author, likes, created} = post;
+    domPost.innerHTML = ` 
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${author.image}" alt="photo's ${author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${author.name}</div>
+                <div class="post-meta__time">${created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="${media}">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+    </div> 
+
+    `
     domPost.classList.add('post');
     myContainer.append(domPost);
 });
